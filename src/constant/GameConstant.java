baff5a140327944b8f6f0312012677e5e3f918a2
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-04-28 15:19:18 +0600

    first commit. project create

---
+++ src/constant/GameConstant.java
@@ -0,0 +1,13 @@
+package constant;
+
+public final class GameConstant {
+    public static final String ALPHABET = "abcdefg";
+    public static final int GRID_LENGTH = 7;
+    public static final int GRID_SIZE = 49;
+    public static final int MAX_ATTEMPTS = 200;
+    public static final int HORIZONTAL_INCREMENT = 1;
+    public static final int VERTICAL_INCREMENT = GRID_LENGTH;
+
+    public GameConstant() {
+    }
+}
